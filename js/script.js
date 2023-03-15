8d9dbb8ac3eac97a2f9b433151b86540d90041d3
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2023-03-15 06:35:11 -0400

    Add inspirational messages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const messageContainer = document.getElementById("container");
 const button = document.getElementById("btn");
 
-const beginning = [];
-const middle = [];
-const end = []
+const message = {
+    beginnings: ["We cannot solve problems", "Learn as if you", "Stay away from those people who try to disparage your ambitions.", "When you give joy to other people,"],
+    middle: ["with the kind of thinking we employed", "will live forever,", "Small minds will always do that", "you get more joy in return."],
+    end: ["when we came up with them.", "live like you will die tomorrow.", " but great minds will give you a feeling that you can become great too.","You should give a good thought to happiness that you can give out."]
+}
+
+function generateMessage() {
+    
+}
